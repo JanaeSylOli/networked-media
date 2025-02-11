63b6f0768a2a6e44825d999ce766e8db1588c3f9
JanaeSylOli <147261259+JanaeSylOli@users.noreply.github.com>
2025-02-11 13:32:51 -0500

    have to update

--- class-demos/class5/script.js
+++ class-demos/class5/script.js
@@ -1,11 +1,29 @@
 window.unload = () => {}
 
+//create an element 
+// 2 modify number as me
+// 1 
+const span = document.createElement('span')
+const node = document.createTextNode('CREATED')
+
+//2 
+span.appendChild(node)
+span. classList.add('stext-body')
+span.
+
 setInterval(time,1000)
 
 function time(){
-    console.log (
-        'second has passed'
-    )
+    // console.log (
+     //   'second has passed'
+   // )
 
-    seconds++
-}
+   const date = new Date()
+
+   console.log (date.toLocaleTimeString)
+
+   // seconds++
+
+Math. randomm() * 100
+let index = Math .floor(Math.random() * 100)
+all spans (i
---
+++ class-demos/class6/script.js
@@ -0,0 +1,24 @@
+window.unload {
+
+}
+b1.add EventListener('click', () => {
+
+    function time(){
+        // console.log (
+         //   'second has passed'
+       // )
+
+       const date = new Date()
+
+       console.log (date.toLocaleTimeString)
+
+       // seconds++
+
+       <div class
+        let div=. document.createElement('div')
+        let c= class div for class list 
+        if[
+            C.DAY 
+            C.NUGH
+            else c
+        ]
